134706cde256df4d5ece06e74db1fc1e19dd08b3
SantosAndrewz <sentosandrewz@gmail.com>
2024-10-30 03:34:57 +0000

    task 1 correction

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,18 +3,18 @@ import _ from "lodash";
 
 //adding elements to the body
 $(document).ready(() => {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<button id='button-1' >Click here to get started</button>");
-  $("body").append("<p id='count'></p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="button-1">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
   let count = 0;
   
   function updateCounter() {
     count += 1;
-    $( "#count" ).text(`${count} clicks on the button`);
+    $('#count').text(`${count} clicks on the button`);
 }
 
-  $( "#button-1" ).on('click', _.debounce(updateCounter, 500, {leading: true, trailing: false}));
+  $('#button-1').on('click', _.debounce(updateCounter, 500, {leading: true, trailing: false}));
 });
