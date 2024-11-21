226c92cb60138f7cebbbef3354fc926f986f7f38
SantosAndrewz <sentosandrewz@gmail.com>
2024-11-21 18:09:20 +0300

    concat

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,8 @@
+import { List, Map} from 'immutable';
+/**
+ * Script provides 2 functions from Immutable.js that combine lists or arrays immutably.
+ */
+
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
+
+export const mergeElements = (page1, page2) => Map(page1).concat(Map(page2));
