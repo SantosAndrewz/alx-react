0463e8dcad0ba81ceef47a2408cff7a43f80e9cb
SantosAndrewz <sentosandrewz@gmail.com>
2024-11-21 14:46:11 +0300

    fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,11 +1,5 @@
 import { fromJS } from "immutable";
 
-/**
- * Script converts an object into an immutable Map.
- * @param {object} object - The object to be converted.
- * @return {Immutable.Map} - The immutable Map formed.
- */
-
 export default function getImmutableObject(object) {
     return fromJS(object);
 }
