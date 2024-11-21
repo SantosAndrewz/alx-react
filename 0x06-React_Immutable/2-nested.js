bd277f08850d7bc16d1f01e2b76bfabae9cb8ba1
SantosAndrewz <sentosandrewz@gmail.com>
2024-11-21 16:24:05 +0300

    access nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,6 @@
+import { fromJS } from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+    const mappedObject = fromJS(object);
+    return mappedObject.getIn(array, undefined);
+}
