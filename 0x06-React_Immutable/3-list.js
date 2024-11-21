03253950990a26efb9c2c6674340aa9ee0d453ba
SantosAndrewz <sentosandrewz@gmail.com>
2024-11-21 16:55:40 +0300

    used list and push

---
+++ 0x06-React_Immutable/3-list.js
@@ -0,0 +1,20 @@
+import { List } from 'immutable';
+
+/**
+ * Scripts converts an array into an Immutable List.
+ * @param {Array} array - The array to be converted into an Immutable List.
+ * @return {List} - The Immutable list formed.
+ */
+export function getListObject(array) {
+    return List(array);
+}
+
+/**
+ * Function adds a string Element to an existing Immutable list.
+ * @param {List} list - The immutable list to which the element is to be added.
+ * @param {string} element - The string element to be added to the list.
+ * @return {List} - New Immutable list formed.
+ */
+export function addElementToList(list, element) {
+    return list.push(element);
+}
