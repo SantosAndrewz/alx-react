4c2615a71a8800342f1669a02554f165fa93ed8a
SantosAndrewz <sentosandrewz@gmail.com>
2024-11-22 02:40:00 +0300

    used -mergeDeep- and -Map-

---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,8 @@
+import { Map } from 'immutable';
+
+/**
+ * Script for deeply merging 2 objects using Map and mergeDeep from Immutable.js
+ */
+export default function mergeDeeplyElements(page1, page2) {
+    return Map(page1).mergeDeep(Map(page2));
+}
